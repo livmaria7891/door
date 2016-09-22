2268f03a900194e21cb78ba5522fb84a42dd84bf
Olivia <reach.olivialegge@gmail.com>
2016-09-22 12:49:00 -0400

    Added a tests to check for edge cases for write_inscription, 8 tests pass

--- doors.rb
+++ doors.rb
@@ -1,11 +1,30 @@
 #doors.rb
 class Door
+
+  attr_accessor :locked, :closed
+  attr_reader :inscription
+
   def initialize(inscription = "", locked=true,closed=true)
     if inscription.class != String || !(locked == true || locked == false) || !(closed == true || closed == false)
       raise ArgumentError
     end
-    @inscription = inscription
+
     @locked = locked
     @closed = closed
+    @inscription = inscription
   end
+
+  def write_inscription(an_inscription)
+    unless inscription == ""
+      raise ArgumentError
+
+    else
+     if an_inscription.class == String
+        @inscription = an_inscription
+      else
+        raise ArgumentError
+     end
+    end
+  end
+
 end
--- specs/door_spec.rb
+++ specs/door_spec.rb
@@ -44,122 +44,139 @@ describe "Testing Doors" do
     expect(tst.inscription).must_equal("")
   end
 
-  it "Checks that inscription cannot be changed" do
-    skip
-    tst=Door.new("Keep this Inscription")
-    tst.inscription = "This Inscription Cannot Be Here"
-    expect(tst.inscription.must_equal("Keep this Inscription")
+  it "Checks that an inscription can be written if one is not already set" do
+    tst=Door.new()
+    tst.write_inscription("An Inscription")
+    tst.inscription.must_equal("An Inscription")
   end
 
-  it "Checks that a door can only be opened if it is unlocked and closed" do
-    skip
-    unlocked_and_closed = Door.new("Room",false,true)
-    expect(unlocked_and_closed.open_door.closed.must_equal(false)
-
-    locked_and_closed = Door.new("Room",true,true)
-    expect(locked_and_closed.open_door.assert_raises(Exception)
-    expect(locked_and_closed.closed.must_equal(true)
-
-    unlocked_and_open = Door.new("Room",false,false)
-    expect(unlocked_and_open.open_door.assert_raises(Exception)
-    expect(locked_and_open.closed.must_equal(false)
-
-    locked_and_open = Door.new("Room",true,false)
-    expect(locked_and_open.open_door.assert_raises(Exception)
-    expect(locked_and_open.closed.must_equal(true)
+  it "Checks that inscription cannot be changed once set" do
+    #skip
+    tst=Door.new("Keep this Inscription",false,false)
+    assert_raises(ArgumentError) {tst.write_inscription("Can't write this.") }
   end
 
-  it "Checks that a door can only be closed if it is not closed" do
-    skip
-    unlocked_and_open = Door.new("Room",false,false)
-    expect(unlocked_and_open.close_door.closed.must_equal(true)
+  # it "Checks that .write_inscription() will only take a string as a parameter" do
+  #   tst=Door.new()
+  #   assert_raises(ArgumentError) { tst.write_inscription(4) }
+  #
+  #   tst=Door.new()
+  #   assert_raises(ArgumentError) { tst.write_inscription(true) }
+  #
+  #   tst=Door.new()
+  #   assert_raises(ArgumentError) { tst.write_inscription(:symbol) }
+  #
+  # end
 
-    unlocked_and_closed = Door.new("Room",false,true)
-    expect(locked_and_closed.close_door.assert_raises(Exception)
-    expect(locked_and_open.closed.must_equal(true)
-
-    locked_and_closed = Door.new("Room",true,true)
-    expect(locked_and_closed.close_door.assert_raises(Exception)
-    expect(locked_and_closed.closed.must_equal(true)
-
-    locked_and_open = Door.new("Room",true,false)
-    expect(locked_and_open.close_door.closed.must_equal(true)
-  end
-
-  it "Checks that a door can only be locked if it is unlocked" do
-    skip
-    unlocked_and_open = Door.new("Room",false,false)
-    expect(unlocked_and_open.lock.locked.must_equal(true)
-
-    unlocked_and_closed = Door.new("Room",false,true)
-    expect(locked_and_open.lock.locked.must_equal(true)
-
-    locked_and_closed = Door.new("Room",true,true)
-    expect(locked_and_closed.lock.assert_raises(Exception)
-    expect(locked_and_closed.locked.must_equal(true)
-
-    locked_and_open = Door.new("Room",true,false)
-    expect(locked_and_open.lock.assert_raises(Exception)
-    expect(locked_and_open.lock.locked.must_equal(true)
-  end
-
-  it "Checks that a door can only be unlocked if it is locked" do
-    skip
-    locked_and_closed = Door.new("Room",true,true)
-    expect(locked_and_closed.unlock.locked.must_equal(false)
-
-    locked_and_open = Door.new("Room",true,false)
-    expect(locked_and_open.unlock.locked.must_equal(false)
-
-    unlocked_and_open = Door.new("Room",false,false)
-    expect(locked_and_closed.unlock.assert_raises(Exception)
-    expect(unlocked_and_open.locked.must_equal(false)
-
-    unlocked_and_closed = Door.new("Room",false,true)
-    expect(locked_and_closed.unlock.assert_raises(Exception)
-    expect(locked_and_open.locked.must_equal(false)
-
-  end
-
-  it "Checks whether or not a door is closed" do
-    skip
-    locked_and_closed = Door.new("Room",true,true)
-    expect(locked_and_closed.closed?).must_equal("This door is closed")
-
-    unlocked_and_closed = Door.new("Room",false,true)
-    expect(unlocked_and_closed.closed?).must_equal("This door is closed")
-
-    locked_and_open = Door.new("Room",true,false)
-    expect(locked_and_open.closed?).must_equal("This door is open")
-
-    unlocked_and_open = Door.new("Room",false,false)
-    expect(unlocked_and_open.closed?).must_equal("This door is open")
-
-  end
-
-  it "Checks whether or not a door is locked" do
-    skip
-    locked_and_closed = Door.new("Room",true,true)
-    expect(locked_and_closed.locked?).must_equal("This door is locked")
-
-    unlocked_and_closed = Door.new("Room",false,true)
-    expect(unlocked_and_closed.locked?).must_equal("This door is unlocked")
-
-    locked_and_open = Door.new("Room",true,false)
-    expect(locked_and_open.locked?).must_equal("This door is locked")
-
-    unlocked_and_open = Door.new("Room",false,false)
-    expect(unlocked_and_open.locked?).must_equal("This door is unlocked")
-
-  end
-
-  it "Shows the inscription" do
-    skip
-    unlocked_and_open = Door.new("An Inscription",false,false)
-    expect(unlocked_and_open.show_inscription).must_equal("An Inscription")
-
-    locked_and_open = Door.new("",true,false)
-    expect(locked_and_open.read_inscription).must_equal("There is no inscription on this door")
-  end
+  # it "Checks that a door can only be opened if it is unlocked and closed" do
+  #   skip
+  #   unlocked_and_closed = Door.new("Room",false,true)
+  #   expect(unlocked_and_closed.open_door.closed.must_equal(false)
+  #
+  #   locked_and_closed = Door.new("Room",true,true)
+  #   expect(locked_and_closed.open_door.assert_raises(Exception)
+  #   expect(locked_and_closed.closed.must_equal(true)
+  #
+  #   unlocked_and_open = Door.new("Room",false,false)
+  #   expect(unlocked_and_open.open_door.assert_raises(Exception)
+  #   expect(locked_and_open.closed.must_equal(false)
+  #
+  #   locked_and_open = Door.new("Room",true,false)
+  #   expect(locked_and_open.open_door.assert_raises(Exception)
+  #   expect(locked_and_open.closed.must_equal(true)
+  # end
+  #
+  # it "Checks that a door can only be closed if it is not closed" do
+  #   skip
+  #   unlocked_and_open = Door.new("Room",false,false)
+  #   expect(unlocked_and_open.close_door.closed.must_equal(true)
+  #
+  #   unlocked_and_closed = Door.new("Room",false,true)
+  #   expect(locked_and_closed.close_door.assert_raises(Exception)
+  #   expect(locked_and_open.closed.must_equal(true)
+  #
+  #   locked_and_closed = Door.new("Room",true,true)
+  #   expect(locked_and_closed.close_door.assert_raises(Exception)
+  #   expect(locked_and_closed.closed.must_equal(true)
+  #
+  #   locked_and_open = Door.new("Room",true,false)
+  #   expect(locked_and_open.close_door.closed.must_equal(true)
+  # end
+  #
+  # it "Checks that a door can only be locked if it is unlocked" do
+  #   skip
+  #   unlocked_and_open = Door.new("Room",false,false)
+  #   expect(unlocked_and_open.lock.locked.must_equal(true)
+  #
+  #   unlocked_and_closed = Door.new("Room",false,true)
+  #   expect(locked_and_open.lock.locked.must_equal(true)
+  #
+  #   locked_and_closed = Door.new("Room",true,true)
+  #   expect(locked_and_closed.lock.assert_raises(Exception)
+  #   expect(locked_and_closed.locked.must_equal(true)
+  #
+  #   locked_and_open = Door.new("Room",true,false)
+  #   expect(locked_and_open.lock.assert_raises(Exception)
+  #   expect(locked_and_open.lock.locked.must_equal(true)
+  # end
+  #
+  # it "Checks that a door can only be unlocked if it is locked" do
+  #   skip
+  #   locked_and_closed = Door.new("Room",true,true)
+  #   expect(locked_and_closed.unlock.locked.must_equal(false)
+  #
+  #   locked_and_open = Door.new("Room",true,false)
+  #   expect(locked_and_open.unlock.locked.must_equal(false)
+  #
+  #   unlocked_and_open = Door.new("Room",false,false)
+  #   expect(locked_and_closed.unlock.assert_raises(Exception)
+  #   expect(unlocked_and_open.locked.must_equal(false)
+  #
+  #   unlocked_and_closed = Door.new("Room",false,true)
+  #   expect(locked_and_closed.unlock.assert_raises(Exception)
+  #   expect(locked_and_open.locked.must_equal(false)
+  #
+  # end
+  #
+  # it "Checks whether or not a door is closed" do
+  #   skip
+  #   locked_and_closed = Door.new("Room",true,true)
+  #   expect(locked_and_closed.closed?).must_equal("This door is closed")
+  #
+  #   unlocked_and_closed = Door.new("Room",false,true)
+  #   expect(unlocked_and_closed.closed?).must_equal("This door is closed")
+  #
+  #   locked_and_open = Door.new("Room",true,false)
+  #   expect(locked_and_open.closed?).must_equal("This door is open")
+  #
+  #   unlocked_and_open = Door.new("Room",false,false)
+  #   expect(unlocked_and_open.closed?).must_equal("This door is open")
+  #
+  # end
+  #
+  # it "Checks whether or not a door is locked" do
+  #   skip
+  #   locked_and_closed = Door.new("Room",true,true)
+  #   expect(locked_and_closed.locked?).must_equal("This door is locked")
+  #
+  #   unlocked_and_closed = Door.new("Room",false,true)
+  #   expect(unlocked_and_closed.locked?).must_equal("This door is unlocked")
+  #
+  #   locked_and_open = Door.new("Room",true,false)
+  #   expect(locked_and_open.locked?).must_equal("This door is locked")
+  #
+  #   unlocked_and_open = Door.new("Room",false,false)
+  #   expect(unlocked_and_open.locked?).must_equal("This door is unlocked")
+  #
+  # end
+  #
+  # it "Shows the inscription" do
+  #   skip
+  #   unlocked_and_open = Door.new("An Inscription",false,false)
+  #   expect(unlocked_and_open.show_inscription).must_equal("An Inscription")
+  #
+  #   locked_and_open = Door.new("",true,false)
+  #   expect(locked_and_open.read_inscription).must_equal("There is no inscription on this door")
+  # end
 
 end
